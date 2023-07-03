fe666531e65e5d9f02baf1d4c8834ca828ee1139
Marcelo Pottier <89404517+marcelopottier@users.noreply.github.com>
2023-07-03 20:24:09 -0300

    Add files via upload

--- assets/api.js
+++ assets/api.js
@@ -1,3 +1,18 @@
-function random(){
-    'https://http.cat/'
+function getImage(){
+    let input = document.getElementById("code-text").value;
+    let s = fetchImage(input);
+    let div = document.getElementById("img-input");
+    //div.src = s;
+    console.log(s);
+}
+
+
+async function fetchImage(input){
+    const response = await fetch(`https://http.cat/${input}`, {
+        method : "GET",
+        mode: 'no-cors'
+    })
+    if(response.blob){
+        return response.blob;
+    }
 }
